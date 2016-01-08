ae59d712f195844faace82962e40ca0bfd82cdb2
Horace Williams <horace.d.williams@gmail.com>
2016-01-07 21:34:48 -0700

    structure and first basic examples for tcp spec

---
+++ spec/protocol_test.rb
@@ -0,0 +1,47 @@
+require "minitest"
+require "minitest/autorun"
+require "minitest/spec"
+require "socket"
+require "json"
+
+describe "Clarke Coin Protocol Spec" do
+  attr_reader :port
+
+  before do
+    @port = ARGV[0] || 8334
+  end
+
+  def symify_json(json_string)
+    JSON.parse(json_string).map do |k,v|
+      [k.to_sym, v]
+    end.to_h
+  end
+
+  def transmit_and_recv(data)
+    s = TCPSocket.new("localhost", port)
+    payload = data.to_json + "\n\n"
+    s.write(payload)
+    response = s.read
+    s.close
+    symify_json(response)
+  end
+
+  it "echos messages" do
+    msg = {message_type: "echo", payload: "pizza"}
+    assert_equal msg, transmit_and_recv(msg)
+  end
+
+  it "lists peers" do
+    msg = {message_type: "list_peers"}
+    resp = transmit_and_recv(msg)
+    assert_equal [], resp[:payload]
+  end
+
+  it "adds a peer" do
+  end
+
+  it "pizza" do
+    assert port
+    assert true
+  end
+end
